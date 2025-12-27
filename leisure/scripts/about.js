250b58543a26a0b2b84d5c0b2ade1fb55e96ee95
Aaliya <aagbetoba@byupathway.edu>
2025-12-27 13:52:53 +0100

    \commit

--- leisure/scripts/about.js
+++ leisure/scripts/about.js
@@ -1,3 +1,14 @@
 let year = document.querySelector('.year');
 const today = new Date()
 year.innerHTML = `${today.getFullYear()}`;
+
+const navigator = document.querySelector('.navigate');
+const  menu = document.querySelector('#dropdown');
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('open');
+    navigator.classList.toggle('open');
+})
+
+document.querySelector('.btn').addEventListener('click',()=>{
+    window.location.href = 'index.html'
+});
